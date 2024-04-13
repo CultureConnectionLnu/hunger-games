c5ad2c77c8cc360f3ebfe68e1621cb7f3b359d13
Nico Vogel <nico-vogel97@outlook.de>
2024-04-13 18:54:35 +0200

    introduce db setup job

---
+++ setup-env.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ -d env ]; then
+    exit 0
+fi
+
+# get the environment variables
+git clone https://github.com/CultureConnectionLnu/hunger-games-env.git env
+
+# only copy the .env file if it does not already exist in the app folder
+if [ ! -f app/.env ]; then
+    cp env/.env app/.env
+fi
