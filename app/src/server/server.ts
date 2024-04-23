c75205133522106743a9ea628111549b6834b038
Nico Vogel <nico-vogel97@outlook.de>
2024-04-23 11:59:50 +0200

    sync clerk users with db upon startup

--- app/src/server/server.ts
+++ app/src/server/server.ts
@@ -7,6 +7,7 @@ import { env } from "~/env";
 import { db } from "./db";
 import { fight } from "./db/schema";
 import { bootstrapWS } from "./wssServer";
+import { UserHandler } from "./api/logic/user";
 
 const port = parseInt(env.PORT);
 const dev = env.NEXT_PUBLIC_NODE_ENV !== "production";
@@ -40,10 +41,58 @@ void app.prepare().then(() => {
       x.count,
     );
   });
+  void syncUsersWithClerk().then(() => {
+    console.log("Clerk sync complete");
+  });
 });
 
 async function removeNotFinishedFights() {
   return db.delete(fight).where(isNull(fight.winner));
+}
+
+async function syncUsersWithClerk() {
+  const users = await UserHandler.instance.getAllClerkUsers();
+  if (users.success === false) {
+    console.error("Failed to get users from clerk");
+    return;
+  }
+
+  const storedUsers = await UserHandler.instance.getAllUsers();
+  const storedUserIds = new Set(storedUsers.map((u) => u.clerkId));
+  await addNewUsers(
+    users.users
+      .filter((u) => !storedUserIds.has(u.userId))
+      .map((u) => u.userId),
+  );
+
+  const userIds = new Set(users.users.map((u) => u.userId));
+  await removeOutdatedUsers(
+    storedUsers.filter((u) => !userIds.has(u.clerkId)).map((u) => u.clerkId),
+  );
+}
+
+async function addNewUsers(newUsers: string[]) {
+  if (newUsers.length === 0) {
+    console.log("No new users to sync");
+    return;
+  }
+
+  console.log("New users to sync: ", newUsers.length);
+  for (const newUser of newUsers) {
+    await UserHandler.instance.createUser(newUser);
+  }
+}
+
+async function removeOutdatedUsers(outdatedUsers: string[]) {
+  if (outdatedUsers.length === 0) {
+    console.log("No outdated users to remove");
+    return;
+  }
+
+  console.log("Outdated users to remove: ", outdatedUsers.length);
+  for (const outdatedUser of outdatedUsers) {
+    await UserHandler.instance.deleteUser(outdatedUser);
+  }
 }
 
 function logFeaturesFlags() {
