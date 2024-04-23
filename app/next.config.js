ff2db71edc00400412d56afb0dc4ce509b13311e
Nico Vogel <nico-vogel97@outlook.de>
2024-04-23 23:38:01 +0200

    run type check as a separate gate

--- app/next.config.js
+++ app/next.config.js
@@ -7,6 +7,9 @@ await import("./src/env.js");
 /** @type {import("next").NextConfig} */
 const config = {
   output: "standalone",
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
