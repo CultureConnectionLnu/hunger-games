65b135cd1eb3d48854c7102257fcad8b162e2e47
Nico Vogel <nico-vogel97@outlook.de>
2024-05-07 22:52:59 +0200

    remove todo list

--- app/src/app/moderator/quest/page.tsx
+++ app/src/app/moderator/quest/page.tsx
@@ -8,16 +8,6 @@ export default async function UsersOverview() {
   const quests = await api.quest.getOngoingQuestsForModerator.query();
   const users = await api.user.allUsers.query();
 
-  /**
-   * next steps:
-   * - create a dialog that shows the selected user
-   * - in the dialog it should show the current quest status of the user
-   *   1. quest not related to this hub (should show where the user needs to go)
-   *   2. provide button to mark the current hub as completed
-   *   3. provide button to assign a new quest to user (for each quest type one button)
-   *      - should have an alert dialog to confirm the assignment to avoid accidental assignments
-   */
-
   return (
     <div className="flex h-full flex-col gap-4 pb-4">
       <div className="flex-grow"></div>
