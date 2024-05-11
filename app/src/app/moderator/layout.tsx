3a730a9270faf74a140857857349278821a71623
Nico Vogel <nico-vogel97@outlook.de>
2024-05-11 08:22:35 +0200

    mark admin and moderator pages as dynamic

--- app/src/app/moderator/layout.tsx
+++ app/src/app/moderator/layout.tsx
@@ -1,12 +1,16 @@
 import { notFound } from "next/navigation";
 import { userHandler } from "~/server/api/logic/handler";
 
+export const dynamic = "force-dynamic";
+
 export default async function AdminLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  if (!(await userHandler.checkRole(["moderator", "medic"]))) {
+  console.log("layout moderator");
+  const authResult = await userHandler.checkRole(["moderator", "medic"]);
+  if (!authResult) {
     notFound();
   }
   return <>{children}</>;
--- app/src/app/moderator/medic/page.tsx
+++ app/src/app/moderator/medic/page.tsx
@@ -2,6 +2,8 @@ import { FindWounded } from "./_components/find-wounded";
 import { WoundedPlayerDialog } from "./_components/wounded-dialog";
 import { WoundedTable } from "./_components/wounded-table";
 
+export const dynamic = "force-dynamic";
+
 export default async function MedicOverview() {
   return (
     <div className="flex h-full flex-col gap-4 pb-4">
