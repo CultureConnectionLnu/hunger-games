7bf5fd622a575c4f004380329cb2322cd6cb9305
Nico Vogel <nico-vogel97@outlook.de>
2024-05-14 23:02:28 +0200

    fix input timer

--- app/src/server/api/logic/games/om.ts
+++ app/src/server/api/logic/games/om.ts
@@ -238,6 +238,7 @@ export class OMGame
         player.id,
       );
     });
+    this.timerHandler.startTimer("input-timer");
   }
 
   private showPattern() {
--- app/test/trpc/om.ts
+++ app/test/trpc/om.ts
@@ -52,17 +52,63 @@ export const omTests = () =>
             ).toEqual("show-pattern");
           },
         ));
+
+      it("once the 'show-timer' is done, it should show the 'input-pattern' view", () =>
+        testFight(async ({ startGame, timer, firstRpsListener }) => {
+          await startGame();
+          timer.getFirstByName("show-timer").emitTimeout();
+
+          expect(
+            getLastEventOf(firstRpsListener, "enable-input")?.view,
+          ).toEqual("input-pattern");
+        }));
+
+      it("when the user clicks on any card, the view should change to 'wait-for-opponent'", () =>
+        testFight(async ({ startGame, timer, clickCard, firstRpsListener }) => {
+          await startGame();
+          timer.getFirstByName("show-timer").emitTimeout();
+          await clickCard("test_user_1", { row: 0, col: 0 });
+
+          expect(
+            getLastEventOf(firstRpsListener, "show-waiting")?.view,
+          ).toEqual("wait-for-opponent");
+        }));
+
+      it('when both players have clicked on the correct card, then the view should change to "show-result"', () =>
+        testFight(async ({ startGame, timer, clickCard, firstRpsListener }) => {
+          await startGame();
+          timer.getFirstByName("show-timer").emitTimeout();
+          const toBeClicked = getLastEventOf(firstRpsListener, "show-pattern")!
+            .data.pattern[0]!;
+          await clickCard("test_user_1", toBeClicked);
+          await clickCard("test_user_2", toBeClicked);
+
+          expect(getLastEventOf(firstRpsListener, "show-result")?.view).toEqual(
+            "show-result",
+          );
+        }));
     });
 
     describe("Timers", () => {
-      //   it("should start choose timer", () =>
-      //     testFight(async ({ startGame, timer, firstRpsListener }) => {
-      //       await startGame();
-      //       expect(() => timer.getLastByName("choose-timer")).not.toThrow();
-      //       expect(
-      //         getLastEventOf(firstRpsListener, "choose-timer")?.data.secondsLeft,
-      //       ).toBeGreaterThan(0);
-      //     }));
+      it("should start show timer", () =>
+        testFight(async ({ startGame, timer, firstRpsListener }) => {
+          await startGame();
+          expect(() => timer.getLastByName("show-timer")).not.toThrow();
+          expect(
+            getLastEventOf(firstRpsListener, "show-timer")?.data.secondsLeft,
+          ).toBeGreaterThan(0);
+        }));
+
+      it("should show input timer when input starts", () =>
+        testFight(async ({ startGame, timer, firstRpsListener }) => {
+          await startGame();
+          timer.getFirstByName("show-timer").emitTimeout();
+          expect(() => timer.getLastByName("input-timer")).not.toThrow();
+          await runAllMacroTasks();
+          expect(
+            getLastEventOf(firstRpsListener, "input-timer")?.data.secondsLeft,
+          ).toBeGreaterThan(0);
+        }));
     });
   });
 
