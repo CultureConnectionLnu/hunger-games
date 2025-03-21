cbb0c25d65645db95a18326b5f0f2f006d1ece54
Nico Vogel <nico-vogel97@outlook.de>
2025-03-21 18:42:24 +0100

    run tests within a file sequentially

--- nextjs/src/server/integration.spec.ts
+++
@@ -1,8 +0,0 @@
-import { webSocketConnectionTests } from "./ws/web-socket-connection_spec";
-
-/**
- * All tests within one file are executed sequentially.
- * Because the services are global, it's not possible to run them in parallel.
- * So, all integration tests need to be imported here to be executed.
- */
-webSocketConnectionTests();
